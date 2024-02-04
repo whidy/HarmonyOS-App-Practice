b95c618b77878415a0f5875256cf1e1e82115b5a
baishun <baishun@wodcloud.com>
2024-02-04 16:05:42 +0800

    some simple pages

---
+++ entry/src/main/ets/pages/Index.js
@@ -0,0 +1,8 @@
+import router from '@ohos.router';
+struct Index extends   {
+    constructor() { }
+    build() {
+            .height('100%');
+    }
+}
+//# sourceMappingURL=Index.js.map
---
+++ entry/src/main/ets/pages/Todo.js
@@ -0,0 +1,11 @@
+struct Todo extends   {
+    constructor() { }
+    build() {
+            .height("600")
+            .padding({ left: 20, right: 20 })
+            .margin(10)
+            .border({ width: 1, style: BorderStyle.Solid, color: Color.Blue, radius: 4 });
+    }
+}
+export {};
+//# sourceMappingURL=Todo.js.map
